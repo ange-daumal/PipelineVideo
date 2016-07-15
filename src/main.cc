debf478fffdb48063a3696e4d0b21b2fbc011736
Laure Daumal <daumal_l@epita.fr>
2016-07-15 14:52:14 +0200

    git rebase lol

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,6 @@
 #include <opencv2/opencv.hpp>
 #include <chrono>
 
-#include "boost/program_options.hpp"
 #include "pipeline.hh"
 #include "filters.hh"
 
@@ -10,67 +9,18 @@ using htp = std::chrono::high_resolution_clock::time_point;
 using hn = std::chrono::high_resolution_clock;
 using namespace std::chrono;
 
-// Program options
-tbb::pipeline load_options(int argc, char** argv) {
-  boost::program_options::options_description desc("Allowed options");
-  desc.add_options()
-    ("help", "produce help message")
-    ("blurr", "use blurr filter")
-    ("contrast", "use contrast filter")
-    ("strengthen", "strengthen edges")
-    ("detect", "use edge detection filter")
-    ("emboss", "use emboss filter")
-    ;
-
-  boost::program_options::variables_map vm;
-  boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
-  boost::program_options::notify(vm);
-
-  if (vm.count("help")) {
-    std::cout << desc << "\n";
-    exit(1);
-  }
-
+int main(void/*int argc, char** argv */)
+{
   cv::namedWindow("Picture");
   tbb::pipeline pipeline;
 
   Initializer init;
-  pipeline.add_filter(init);
-
-  if (vm.count("blurr")) {
-    Blurr blurr;
-    pipeline.add_filter(blurr);
-  }
-
-  if (vm.count("contrast")) {
-    Contrast contrast;
-    pipeline.add_filter(contrast);
-  }
-
-  if (vm.count("strengthen")) {
-    StrengthenEdge strengthen;
-    pipeline.add_filter(strengthen);
-  }
-
-  if (vm.count("detect")) {
-    DetectEdge detect;
-    pipeline.add_filter(detect);
-  }
-
-  if (vm.count("emboss")) {
-    Emboss emboss;
-    pipeline.add_filter(emboss);
-  }
-
+  Blurr blurr;
+  Contrast contrast;
+  StrengthenEdge strengthen;
+  DetectEdge detect;
+  Emboss emboss;
   Terminator term;
-  pipeline.add_filter(term);
-
-  return pipeline;
-}
-
-int main(int argc, char** argv)
-{
-  auto pipeline = load_options(argc, argv);
 
   htp t1 = hn::now();
   //serial version
@@ -83,7 +33,17 @@ int main(int argc, char** argv)
   auto s_time = duration_cast<milliseconds>(t2-t1).count();
   auto p_time = duration_cast<milliseconds>(t4-t3).count();
 
+  pipeline.add_filter(init);
+  //pipeline.add_filter(emboss);
+  //pipeline.add_filter(detect);
+  //pipeline.add_filter(strengthen);
+  //pipeline.add_filter(contrast);
+  pipeline.add_filter(blurr);
+  pipeline.add_filter(term);
+
   pipeline.run(3);
+
+  printf("Over\n");
 
   return 0;
 }
