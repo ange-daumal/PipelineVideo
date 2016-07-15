0c317c28e1c40d046e55a942efd9de08ab23eb1a
MENDOZA <rogelio.mendoza@epita.fr>
2016-07-15 10:15:32 +0200

    Added bin to .gitignore, added value to compare time of execution in main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,13 @@
 #include <tbb/tbb.h>
 #include <opencv2/opencv.hpp>
+#include <chrono>
 
 #include "pipeline.hh"
 #include "filters.hh"
+
+using htp = std::chrono::high_resolution_clock::time_point;
+using hn = std::chrono::high_resolution_clock;
+using namespace std::chrono;
 
 int main(void/*int argc, char** argv */)
 {
@@ -16,6 +21,17 @@ int main(void/*int argc, char** argv */)
   DetectEdge detect;
   Emboss emboss;
   Terminator term;
+
+  htp t1 = hn::now();
+  //serial version
+  htp t2 = hn::now();
+
+  htp t3 = hn::now();
+  //parallel version
+  htp t4 = hn::now();
+
+  auto s_time = duration_cast<milliseconds>(t2-t1).count();
+  auto p_time = duration_cast<milliseconds>(t4-t3).count();
 
   pipeline.add_filter(init);
   pipeline.add_filter(emboss);
